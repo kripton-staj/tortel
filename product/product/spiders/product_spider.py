2d4c82d58eafdd5391f98eb9b45ec5002059bb25
Selin Kurt <seliin.kurttt@gmail.com>
2021-03-17 17:42:59 +0300

    extract product-description by script tags

--- product/product/spiders/product_spider.py
+++ product/product/spiders/product_spider.py
@@ -27,17 +27,38 @@ class ProductSpider(scrapy.Spider):
 
         url = response.url
         title = response.xpath('//title/text()').get()
-        description = response.xpath('//div[@class="product-details"]/p[last()]/text()').get()
-        breadcrumbs = response.xpath('//nav[@class="breadcrumbs-nav"]//a/@href').extract()
+        # breadcrumbs = response.xpath('//nav[@class="breadcrumbs-nav"]//a/@href').extract()
+        # description = response.xpath('//div[contains(@class, "description")]').extract()
 
+        items = response.xpath("//script[contains(., 'description')]/text()")
+        txt = items.extract_first()
+        start = txt.find('description') + 14
+        finish = txt[start::].find('",')
+        description = txt[start:start+finish]
+
+        """"
+        # mediamarkt
+        # description = response.xpath('//div[@class="fg-box bpx0 bpy1 bsx3 bsy1 mpx0 mpy1 msx3 msy1 spx0 spy1 ssx3 ssy1"]').extract()
+        description = response.xpath('//div[@class="product-details"]/p[last()]/text()').get()
+        if not description:
+            # bol.com
+            # description = response.xpath('//div[@class="product-description"]').getall()
+            description = response.xpath('//div[contains(@class, "description")]').extract()
+            if not description:
+                # coolblue.nl
+                description = response.xpath('//div[@class="cms-content hide@md-down"]/p[last()]/text()').getall()
+                if not description:
+                    # wehkamp.nl
+                    description = response.xpath("//script[contains(., 'description')]/text()").extract_first()
+                    # description = response.xpath('/html//div/*[contains(@class, "description")]').extract()
+        """
         product['url'] = url
         product['title'] = title
         product['description'] = description
-        product['breadcrumbs'] = breadcrumbs
 
         product_db = self.s.query(models.ProductPage).filter(models.ProductPage.url.ilike(url)).first()
         if not product_db:
-            product_db = models.ProductPage(url=url, title=title, description=description, breadcrumbs=breadcrumbs)
+            product_db = models.ProductPage(url=url, title=title, description=description)
             self.s.add(product_db)
         else:
             product_db.title = title
