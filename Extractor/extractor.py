9a6b1caac77b1740d656d6ef086e862ba4b257be
Selin Kurt <seliin.kurttt@gmail.com>
2021-04-13 17:53:43 +0300

    extract from script tag for description

--- Extractor/extractor.py
+++ Extractor/extractor.py
@@ -1,3 +1,6 @@
+import json
+
+
 def extract_title(soup):
     return soup.title.text.replace('\\n', '').replace('\\t', '').replace('\\r', '')
 
@@ -14,18 +17,34 @@ def extract_description(soup):
                    {"class": "product-information__wrapper"},  {"class": "std product-info"},
                    {"class": "productDescription__short-description"}, {"itemprop": "description"},
                    {"id": "itemLongDescription"}, {"class": "product-description p-lg-5"},
-                   {"class": "pd-info__usp-list"}]
+                   {"class": "pd-info__usp-list"}, {"class": "product-description"}, {"class": "product-info__text"},
+                   {"id": "product-page"}, {"class": "f-productDetails-table"}, {"id": "tab-description"},
+                   {"class": "mxd-page-section"}, {"class": "product-collateral"}]
 
     for class_name in class_names:
         description = soup.find(attrs=class_name)
         if description:
             description = description.get_text(strip=True)
             description = description.replace('\\n', '').replace('\\t', '').replace('\\r', '')
+            description = " ".join(description.split())
             break
-    try:
-        description = " ".join(description.split())
-    except AttributeError as a:
-        print(a)
+
+    if not description:
+        try:
+            script_tag = soup.find_all('script', {'type': 'application/ld+json'})
+            for script in script_tag:
+                script_str = script.string
+                script = script_str.replace('\\n', '').replace('\\t', '').replace('\\r', '').replace('\\', '')
+                json_script = json.loads(script)
+                if json_script['@type'] == ('Product' or 'product'):
+                    description = json_script['description']
+
+        except TypeError as t:
+            print(t)
+        except ValueError as v:
+            print(v)
+        except KeyError as k:
+            print(k)
 
     return description
 
@@ -34,18 +53,16 @@ def extract_breadcrumbs(soup):
     breadcrumbs = []
     brd_array = [{"class": "breadcrumbs__link"}, {"class": "breadcrumb"}, {"id": "breadcrumb"},
                  {"id": "breadcrumbs"}, {"id": "crumbar-content"}, {"class": "breadcrumbs"},
-                 {"class": "c-breadcrumb"}, {"class": "nav nav-tabs"}]
+                 {"class": "c-breadcrumb"}, {"class": "nav nav-tabs"}, {"class": "woocommerce-breadcrumb"},
+                 {"class": "Breadcrumb-module__root___2XZI0 col-6-m ProductDetailPage-module__breadcrumb___SVrjj"},
+                 {"class": "f-productHeader js-articleHeader"}]
 
     for brd in brd_array:
-        breadcrumbs = soup.find(attrs=brd)
-        if breadcrumbs:
-            breadcrumbs = breadcrumbs.get_text(strip=True)
-            breadcrumbs = breadcrumbs.replace('\\n', '').replace('\\t', '').replace('\\r', '')
+        breadcrumbs_list = soup.find(attrs=brd)
+        if breadcrumbs_list:
+            for a in breadcrumbs_list.find_all('a', href=True):
+                breadcrumbs.append(str(a['href']))
             break
-    try:
-        breadcrumbs = " ".join(breadcrumbs.split())
-    except AttributeError as a:
-        print(a)
 
     return breadcrumbs
 
@@ -58,17 +75,17 @@ def extract_specifications(soup):
                         {"id": "product-specifications"}, {"class": "mobile-tab first body-font-size"},
                         {"id": "eigenschappen"}, {"class": "expert_review_products_shortdesc_left d-block d-lg-none"},
                         {"id": "specifications"}, {"class": "col-12 px-0 px-lg-4 my-2 product-attributes"},
-                        {"class": "feature-benefit__text-wrap"}]
+                        {"class": "feature-benefit__text-wrap"}, {"class": "product__specs"},
+                        {"class": "slot slot--seperated slot--seperated--has-more-content js_slot-specifications"},
+                        {"class": "v-card__text pa-2 product-specifications__text"}, {"class": "std"},
+                        {"id": "Characteristics"}]
 
     for class_name in spec_class_names:
         specification = soup.find(attrs=class_name)
         if specification:
             specification = specification.get_text(strip=True)
             specification = specification.replace('\\n', '').replace('\\t', '').replace('\\r', '')
+            specification = " ".join(specification.split())
             break
-    try:
-        specification = " ".join(specification.split())
-    except AttributeError as a:
-        print(a)
 
     return specification
