36bc20789f28f108e5477181efe72d2669501bfc
Selin Kurt <seliin.kurttt@gmail.com>
2021-04-14 17:29:12 +0300

    added dict object that contains url and ean keys and values

--- spider/spider/spiders/spider.py
+++ spider/spider/spiders/spider.py
@@ -17,24 +17,27 @@ class Spider(scrapy.Spider):
     Session = sessionmaker(bind=engine)
     s = Session()
 
+    with open("results.json", "r") as f:
+        url_and_ean = {}
+        json_list = json.load(f)
+        for url_name in json_list:
+            url_and_ean[url_name['url']] = url_name['ean']
+    print(url_and_ean)
+
     def start_requests(self):
-        with open("results-20210322-162308.json", "r") as f:
-            urls = []
-            json_list = json.load(f)
-            for url_name in json_list:
-                urls.append(url_name['url'])
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-        for url in urls:
-            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+        for key in list(self.url_and_ean.keys()):
+            yield scrapy.Request(url=key, headers=headers, callback=self.parse)
 
     def parse(self, response):
         product = SpiderItem()
         url = response.url
         body = str(response.body)
+        ean = self.url_and_ean[url]
 
         product_db = self.s.query(models.ProductBody).filter(models.ProductBody.url.ilike(url)).first()
         if not product_db:
-            product_db = models.ProductBody(url=url, body=body)
+            product_db = models.ProductBody(url=url, body=body, ean=ean)
             self.s.add(product_db)
 
         self.s.commit()
