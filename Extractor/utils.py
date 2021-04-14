9d07854044fcf0b7319495eb1b7362afa01f5f03
Selin Kurt <seliin.kurttt@gmail.com>
2021-04-14 17:31:40 +0300

    added db query row for get test data by group their ean

--- Extractor/utils.py
+++ Extractor/utils.py
@@ -4,6 +4,7 @@ import extractor
 from models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
 from bs4 import BeautifulSoup
 
 
@@ -16,15 +17,14 @@ s = Session()
 
 def database_operations():
     product_body = s.query(models.ProductBody)
-
     for value in product_body:
         soup = BeautifulSoup(value.body, "lxml")
         url = value.url
+        ean = value.ean
         title = extractor.extract_title(soup)
         description = extractor.extract_description(soup)
         breadcrumbs = extractor.extract_breadcrumbs(soup)
         specifications = extractor.extract_specifications(soup)
-        ean = ""
 
         product_data = s.query(models.ProductData).filter(models.ProductData.url.ilike(url)).first()
         if not product_data:
@@ -32,6 +32,7 @@ def database_operations():
                                               breadcrumbs=breadcrumbs, specifications=specifications, ean=ean)
             s.add(product_data)
         else:
+            product_data.ean = ean
             product_data.title = title
             product_data.description = description
             product_data.breadcrumbs = breadcrumbs
@@ -39,6 +40,10 @@ def database_operations():
 
     s.commit()
     s.close()
+
+    result = s.query(func.array_agg(models.ProductData.url),
+                     func.count(models.ProductData.ean)).group_by(models.ProductData.ean).all()
+    print(result)
 
 
 # This function has been written to see how much data we can extract from the given url
