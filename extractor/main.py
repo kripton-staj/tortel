a0b0eafee7357b877653577b621a5ebee6998e0d
Selin Kurt <seliin.kurttt@gmail.com>
2021-04-20 17:30:38 +0300

    comment line removed

--- extractor/main.py
+++ extractor/main.py
@@ -2,7 +2,6 @@ from tortel.extractor.crud import get_data, print_report, write_extracted_data
 
 
 def main():
-    # db den butun product page alip ,textleri alıp 2li comb(math) text similarityi cagirip rapor donsun
     # write_extracted_data
     print_report()
     get_data()
