01567098c8125cd48c691ce40657346b34e4ea79
Selin Kurt <seliin.kurttt@gmail.com>
2021-04-23 12:31:41 +0300

    models.py and config.py moved into common directory

--- extractor/config.py
+++
@@ -1,5 +0,0 @@
-# Scheme: "postgres+psycopg2://
-# <USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-DATABASE_URI = 'postgres+psycopg2://' \
-               'postgres:gizliparola@localhost:5432/product'
