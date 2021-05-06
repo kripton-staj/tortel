d61d01eb8077adba7bda18377a5f4fe88b8cb213
Selin Kurt <seliin.kurttt@gmail.com>
2021-05-06 16:37:17 +0300

    created test functions for the pipeline

--- similarity_checker/pipeline.py
+++ similarity_checker/pipeline.py
@@ -1,40 +1,59 @@
 import spacy
+from nltk.stem.snowball import SnowballStemmer
 
 
-def tokenization(set_product_text):
+def tokenization(product_text):
     """
-    Tokenization, remove punctuation and stop words
-    :param set_product_text: set of all extraction data for each url
-    :return: set of tokens for all extracted data
-    :rtype: class 'set'
+    Separates product text into tokens.
+    :param product_text: set of product_text for two url
+    :return: tokens for two product texts
+    :rtype: class 'list'
     """
 
     nlp = spacy.load("nl_core_news_sm")
 
-    set_tokenization = set({})
-    for product_text in set_product_text:
-        doc1 = nlp(product_text[0])
-        doc2 = nlp(product_text[1])
+    doc1 = nlp(str(product_text[0]))
+    doc2 = nlp(str(product_text[1]))
+    tokens1 = [token.text for token in doc1]
+    tokens2 = [token2.text for token2 in doc2]
+    tokens = [tokens1, tokens2]
 
-        tokens_without_sw1 = [token.text for token in doc1
-                              if not token.is_stop and not token.is_punct]
-        tokens_without_sw2 = [token.text for token in doc2
-                              if not token.is_stop and not token.is_punct]
-        tokens_without_sw = (str(tokens_without_sw1), str(tokens_without_sw2))
-        set_tokenization.add(tokens_without_sw)
-
-    return set_tokenization
+    return tokens
 
 
-def check_cosine_similarity(set_tokenization):
+def remove_stop_words_and_puncts(tokens):
     """
-    WP
+    Removes stop words and punctuations from list of tokens.
+    :param tokens: tokens list for two product texts
+    :return: tokens list without stop words and punctuations
+    :rtype: class 'list'
     """
+
     nlp = spacy.load('nl_core_news_lg')
-    for tokens_tuple in set_tokenization:
-        doc1 = nlp(tokens_tuple[0])
-        doc2 = nlp(tokens_tuple[1])
-        for token1 in doc1:
-            for token2 in doc2:
-                if not token1.is_punct and not token2.is_punct:
-                    print(token1.text, token2.text, token1.similarity(token2))
+    tokens_without_sw_and_punct1 = [token for token in tokens[0]
+                                    if not nlp.vocab[token].is_punct
+                                    and not nlp.vocab[token].is_stop]
+    tokens_without_sw_and_punct2 = [token for token in tokens[1]
+                                    if not nlp.vocab[token].is_punct
+                                    and not nlp.vocab[token].is_stop]
+    tokens_without_sw_and_punct = [tokens_without_sw_and_punct1,
+                                   tokens_without_sw_and_punct2]
+    return tokens_without_sw_and_punct
+
+
+def stemming(tokens_without_sw_and_punct):
+    """
+    Creates a new list of tokens after applying stemming into tokens.
+    :param tokens_without_sw_and_punct: List of tokens without
+                                        stop words and punctuations
+    :return: list of tokens after applying stemming
+    :rtype: class 'list'
+    """
+    stemmer = SnowballStemmer(language='dutch')
+    tokens_after_stemming1 = [stemmer.stem(token) for token
+                              in tokens_without_sw_and_punct[0]]
+    tokens_after_stemming2 = [stemmer.stem(token) for token
+                              in tokens_without_sw_and_punct[1]]
+    tokens_after_stemming = [tokens_after_stemming1,
+                             tokens_after_stemming2]
+    return tokens_after_stemming
--- tests/test_similarity_checker.py
+++ tests/test_similarity_checker.py
@@ -1,51 +1,69 @@
-from tortel.similarity_checker.pipeline import (check_cosine_similarity,
-                                                tokenization)
+from similarity_checker.pipeline import (remove_stop_words_and_puncts,
+                                         stemming, tokenization)
 
 
 class TestSimilarityChecker:
-    def test_similarity_check(self):
-        set_tokens = tokenization(self.test_product_text())
-        check_cosine_similarity(set_tokens)
+    def test_tokenization(self):
+        test_data = ["Rapoo 3920P Mluis Radiografisch Laser Goud,"
+                     " Wit Artikelnummer: 1549450 Fabrikantnummer: 13347"
+                     " EAN: 6943518933474 Technische specificaties"
+                     " Zendmethode Radiografisch Interfaces USB Sensor"
+                     " Laser Sensorresolutie 1600dpi Aantal toetsen",
+                     "Rapoo 3920PGL Draadloze Muis Gegevens De 3920P"
+                     " draadloze muis van Rapoo heeft een betrouwbare 5G"
+                     " draadloze verbinding. Meer informatie Prijs € 22,95"
+                     " Garantietyp"]
 
-    def test_product_text(self):
-        test_data = [{"Rapoo 3920P Muis Radiografisch Laser Goud,"
-                      " Wit Artikelnummer: 1549450 Fabrikantnummer: 13347"
-                      " EAN: 6943518933474 Technische specificaties"
-                      " Zendmethode Radiografisch Interfaces	USB Sensor"
-                      "Laser Sensorresolutie	1600dpi Aantal toetsen"
-                      "6 Kleur (specifiek)	Goud, Wit Gewicht	59g Hoogte"
-                      "34mm Breedte	62mm Diepte	93mm Stroomverzorging"
-                      "2x AA batterij (penlite), meegeleverd Geschikt voor"
-                      "(muis/toetsenbord)	Rechtshandigen Soortnaam"
-                      "Muis this an historic product which is not available,"
-                      "thus not updated anymore"},
-                     {"Rapoo 3920PGL Draadloze Muis Gegevens De 3920P "
-                      "draadloze muis van Rapoo heeft een betrouwbare 5G"
-                      "draadloze verbinding. Maak hem je beste vriend door"
-                      "de knoppen zelf in te stellen met jou favoriete"
-                      "functies.Wireless Laser Mouse Reliable 5G wireless"
-                      "connection Surfree laser engine Powerful customized"
-                      "function Nano receiver 4D scroll wheel Low voltage"
-                      "alarm Reliable 5G wireless connection Reliable 5G"
-                      "wireless transmission far away from the interference"
-                      "of 2.4G wireless,bluetooth and WIFI device. Surfree"
-                      "laser engine Adjustable high-definition laser engine"
-                      "makes your mouse move freely on almost all surfaces"
-                      "(even on glass). Powerful customized function Save time"
-                      "by customized buttons such as Forward,Back,Tilt-wheel,"
-                      "Zoom-in,Zoom-out etc to offer quick responses."
-                      "Meer informatie Prijs € 22,95 Garantietype"
-                      "Carry in Model	3920PGL Fabrikantnummer (MPN) 3920PGL"
-                      "EAN 6943518933474 Merk Rapoo Fabrieksgarantie 2 jaar"}]
+        actual = tokenization(test_data)
+        expected = [['Rapoo', '3920P', 'Mluis', 'Radiografisch', 'Laser',
+                     'Goud', ',', 'Wit', 'Artikelnummer', ':', '1549450',
+                     'Fabrikantnummer', ':', '13347', 'EAN', ':',
+                     '6943518933474', 'Technische', 'specificaties',
+                     'Zendmethode', 'Radiografisch', 'Interfaces', 'USB',
+                     'Sensor', 'Laser', 'Sensorresolutie', '1600dpi',
+                     'Aantal', 'toetsen'],
+                    ['Rapoo', '3920PGL', 'Draadloze', 'Muis', 'Gegevens',
+                     'De', '3920P', 'draadloze', 'muis', 'van', 'Rapoo',
+                     'heeft', 'een', 'betrouwbare', '5', 'G', 'draadloze',
+                     'verbinding', '.', 'Meer', 'informatie', 'Prijs', '€',
+                     '22,95', 'Garantietyp']]
 
-        set_product_text = set({})
-        set1 = (test_data[0], test_data[1])
-        set_product_text.add(set1)
+        assert all([a == b for a, b in zip(actual, expected)])
 
-        return set_product_text
+    def test_remove_stop_words_and_puncts(self):
+        test_data = [['Als', 'u', 'binnen', '2', 'maanden', 'na', 'uw',
+                      'aankoop', 'dit', 'Tamron', 'objectief', 'registreert',
+                      ',', 'krijgt', 'u', 'van', 'Tamron', '5', 'jaar',
+                      'garantie', '.'],
+                     ['Zolang', 'de', 'voorraad', 'strekt', ',', 'uitsluitend',
+                      'op', 'producten', 'verkocht', 'en', 'verzonden', 'door',
+                      'Fnac.be', '.']]
+
+        actual = remove_stop_words_and_puncts(test_data)
+        expected = [['2', 'maanden', 'aankoop', 'Tamron', 'objectief',
+                     'registreert', 'krijgt', 'Tamron', '5', 'jaar',
+                     'garantie'],
+                    ['Zolang', 'voorraad', 'strekt', 'uitsluitend',
+                     'producten', 'verkocht', 'verzonden', 'Fnac.be']]
+
+        assert all([a == b for a, b in zip(actual, expected)])
+
+    def test_stemming(self):
+        test_data = [['55,4', 'maanden', 'aankoop', 'Tamron', 'krijgt',
+                      'jaar'],
+                     ['voorraad', 'strekt', 'uitsluitend', 'producten',
+                      'verzonden', 'Fnac.be']]
+
+        actual = stemming(test_data)
+        expected = [['55,4', 'maand', 'aankop', 'tamron', 'krijgt', 'jar'],
+                    ['voorrad', 'strekt', 'uitsluit', 'product', 'verzond',
+                     'fnac.b']]
+
+        assert all([a == b for a, b in zip(actual, expected)])
 
 
 if __name__ == '__main__':
     test1 = TestSimilarityChecker()
-    test1.test_product_text()
-
+    test1.test_tokenization()
+    test1.test_remove_stop_words_and_puncts()
+    test1.test_stemming()
